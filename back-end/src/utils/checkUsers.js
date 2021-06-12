3881e77c7febde50296bf919d2bb250ef0621cf8
luanerdy <luan.vini1@gmail.com>
2021-06-11 22:20:12 -0300

    logout message

--- back-end/src/utils/checkUsers.js
+++ back-end/src/utils/checkUsers.js
@@ -1,12 +1,29 @@
+import messages from '../data/messages.js';
 import participants from '../data/participants.js';
+import dayjs from 'dayjs';
+
+const logout = (p) => {
+	const message = {
+		from: p.name,
+		to: 'Todos',
+		text: 'sai da sala...',
+		type: 'status',
+		time: dayjs().format('HH:mm:ss'),
+	};
+
+	messages.push(message);
+};
 
 const checkUsers = () => {
-	const newParticipants = participants.filter(
-		(p) => Date.now() - p.lastStatus <= 10000
-	);
-	
+	const newParticipants = participants.filter((p) => {
+		if (Date.now() - p.lastStatus <= 10000) {
+			return true;
+		}
+		logout(p);
+	});
+
 	participants.length = 0;
-	newParticipants.forEach(p => participants.push(p));
+	newParticipants.forEach((p) => participants.push(p));
 };
 
 export default checkUsers;
